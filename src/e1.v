2f54a47594b36ce92461038065d8f0063b2cb93b
Fran Bonino <66426042+akmsw@users.noreply.github.com>
2022-08-17 23:16:26 -0300

    Create e1.v

---
+++ src/e1.v
@@ -0,0 +1,21 @@
+module tarea1
+  (
+    input wire in1,
+    input wire in2,
+    input wire in3,
+    input wire in4,
+
+    output wire out1,
+    output wire out2
+  );
+
+  wire a, b, c, d;
+
+  assign a = in1 & in2;
+  assign b = a | in3;
+  assign c = ~b;
+  assign d = in3 & in4;
+  assign out1 = c | d;
+  assign out2 = d & in4 & in3;
+
+endmodule
